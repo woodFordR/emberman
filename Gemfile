86a0372810f748a9d3988b9dc7de61d803e95f78
woodFordR <adam@kaewell.com>
2024-04-03 14:29:42 -0400

    init commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.5'
+gem 'middleman-autoprefixer', '~> 3.0'
+gem 'pandoc-ruby'
+gem 'terser', '~> 1.1'
+gem 'tilt-handlebars'
+gem 'typescript-node'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
---
+++ config.rb
@@ -0,0 +1,66 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+Middleman::Extensions.register :image_tracker do
+  require "extensions/image_tracker"
+  ImageTracker
+end
+
+activate :image_tracker
+
+activate :external_pipeline,
+  name: :ember,
+  command: "cd frontend/ && ember #{build? ? :build : :serve} --environment #{config[:environment]}",
+  source: "frontend/dist",
+  latency: 2
+
+# activate :external_pipeline,
+#   name: :webpack,
+#   command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
+#   source: ".tmp/dist",
+#   latency: 1
+
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript, compressor: Terser.new
+# end
